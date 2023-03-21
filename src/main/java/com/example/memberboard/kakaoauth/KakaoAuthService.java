a4628cbbc7cf40685b619381df9e83d22166f617
LeeJaeIn <wodls1616@4valuesoft.com>
2023-03-21 09:30:53 +0900

    카카오 인증 삭제 다시 추가할 예정

--- src/main/java/com/example/memberboard/kakaoauth/KakaoAuthService.java
+++
@@ -1,7 +0,0 @@
-package com.example.memberboard.kakaoauth;
-
-public interface KakaoAuthService {
-    String getAuthorizationUrl(String redirectUri);
-    String getAccessToken(String code, String redirectUri);
-    KakaoUserInfo getUserInfo(String accessToken);
-}
